a4cda3a34780aa405621cb796c489fefe8b31a70
Alexandru Țigănașu <alexandru.tiganasu@stud.ubbcluj.ro>
2024-11-19 21:34:40 +0200

    finished inbox design

--- src/section/chat/Inbox.jsx
+++ src/section/chat/Inbox.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import user01 from "../../images/user/user-01.png"
-import { DotsThree } from '@phosphor-icons/react'
+import { DotsThree, LinkSimple, PaperPlaneTilt, Smiley } from '@phosphor-icons/react'
 export default function Inbox() {
   return (
     <div className='  flex h-full flex-col border-l xl:w-3/4
@@ -31,13 +31,94 @@ export default function Inbox() {
         </div>
 
         {/* List of messages */}
-        <div>
+        <div className='max-h-full space-y-3.5 overflow-auto no-scrollbar px-6 py-7.5 grow'>
+            
+            <div className='max-w-125'>
+                <p className='mb-2.5 text-sm font-medium'>Andu Andu</p>
+                <div className='mb-2.5 rounded-2xl rounded-tl-none bg-gray px-5 py-3 dark:bg-boxdark-2'>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim quisquam neque iste dolorum exercitationem nesciunt reiciendis perspiciatis quis, maiores tempore quae, nisi inventore molestiae doloribus officia cumque vitae et eaque.</p>
+                </div>
+                <p className='text-xs'>4:32pm</p>
+            </div>
 
+            <div className='max-w-125 ml-auto'>
+                
+                <div className='mb-2.5 rounded-2xl rounded-br-none bg-primary px-5 py-3'>
+                    <p className='text-white'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim quisquam neque iste dolorum exercitationem nesciunt reiciendis perspiciatis quis, maiores tempore quae, nisi inventore molestiae doloribus officia cumque vitae et eaque.</p>
+                </div>
+                <p className='text-xs'>4:32pm</p>
+            </div>
+
+            <div className='max-w-125'>
+                <p className='mb-2.5 text-sm font-medium'>Andu Andu</p>
+                <div className='mb-2.5 rounded-2xl rounded-tl-none bg-gray px-5 py-3 dark:bg-boxdark-2'>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim quisquam neque iste dolorum exercitationem nesciunt reiciendis perspiciatis quis, maiores tempore quae, nisi inventore molestiae doloribus officia cumque vitae et eaque.</p>
+                </div>
+                <p className='text-xs'>4:32pm</p>
+            </div>
+
+            <div className='max-w-125 ml-auto'>
+                
+                <div className='mb-2.5 rounded-2xl rounded-br-none bg-primary px-5 py-3'>
+                    <p className='text-white'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim quisquam neque iste dolorum exercitationem nesciunt reiciendis perspiciatis quis, maiores tempore quae, nisi inventore molestiae doloribus officia cumque vitae et eaque.</p>
+                </div>
+                <p className='text-xs'>4:32pm</p>
+            </div>
+
+            <div className='max-w-125'>
+                <p className='mb-2.5 text-sm font-medium'>Andu Andu</p>
+                <div className='mb-2.5 rounded-2xl rounded-tl-none bg-gray px-5 py-3 dark:bg-boxdark-2'>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim quisquam neque iste dolorum exercitationem nesciunt reiciendis perspiciatis quis, maiores tempore quae, nisi inventore molestiae doloribus officia cumque vitae et eaque.</p>
+                </div>
+                <p className='text-xs'>4:32pm</p>
+            </div>
+
+            <div className='max-w-125 ml-auto'>
+                
+                <div className='mb-2.5 rounded-2xl rounded-br-none bg-primary px-5 py-3'>
+                    <p className='text-white'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim quisquam neque iste dolorum exercitationem nesciunt reiciendis perspiciatis quis, maiores tempore quae, nisi inventore molestiae doloribus officia cumque vitae et eaque.</p>
+                </div>
+                <p className='text-xs'>4:32pm</p>
+            </div>
+
+            <div className='max-w-125'>
+                <p className='mb-2.5 text-sm font-medium'>Andu Andu</p>
+                <div className='mb-2.5 rounded-2xl rounded-tl-none bg-gray px-5 py-3 dark:bg-boxdark-2'>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim quisquam neque iste dolorum exercitationem nesciunt reiciendis perspiciatis quis, maiores tempore quae, nisi inventore molestiae doloribus officia cumque vitae et eaque.</p>
+                </div>
+                <p className='text-xs'>4:32pm</p>
+            </div>
+
+            <div className='max-w-125 ml-auto'>
+                
+                <div className='mb-2.5 rounded-2xl rounded-br-none bg-primary px-5 py-3'>
+                    <p className='text-white'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim quisquam neque iste dolorum exercitationem nesciunt reiciendis perspiciatis quis, maiores tempore quae, nisi inventore molestiae doloribus officia cumque vitae et eaque.</p>
+                </div>
+                <p className='text-xs'>4:32pm</p>
+            </div>
         </div>
 
         {/* Input */}
-        <div>
-            
+        <div className='sticky bottom-0 border-t border-stroke bg-white px-6 py-5 dark:border-strokedark dark:bg-boxdark'>
+            <form className='flex items-center justify-between space-x-4.5'>
+                <div className='relative w-full'>
+                    <input type="text" placeholder='Type something here' className='h-13 w-full rounded-md border border-stroke bg-gray pl-5 pr-19 text-black placeholder-body outline-none focus:border-primary'/>
+                
+                    <div className='absolute right-5 top-1/2 -translate-y-1/2 items-center justify-end space-x-4'>
+                        <button className='hover:text-primary'>
+                            <LinkSimple size={20}/>
+                        </button>
+
+                        <button className='hover:text-primary'>
+                            <Smiley size={20}/>
+                        </button>
+                    </div>
+                </div>
+
+                <button className='flex items-center justify-center h-13 max-w-13 w-full rounded-md bg-primary text-white hover:bg-opacity-90'>
+                    <PaperPlaneTilt size={24} weight='bold'/>
+                </button>
+            </form>
         </div>
     </div>
   )
